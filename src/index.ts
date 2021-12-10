ee58a89b8949ca92e221665f170b126bf786c912
flowkater <chjw102@gmail.com>
2021-12-10 23:32:39 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { fizzbuzzer_nonfp } from "./fizzbuzz-nonfp";
+
+fizzbuzzer_nonfp();
+
+const app = document.getElementById("app");
+
+if (app !== null) {
+  app.innerHTML = `
+  Hello World
+  `;
+}
